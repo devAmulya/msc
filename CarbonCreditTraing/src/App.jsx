8f3cca96c3b8398ce9a630bf61772b4c607859f7
VANSH PURI <vworkp625@gmail.com>
2025-03-16 01:41:50 +0530

    Adding Navbar and footer

--- CarbonCreditTraing/src/App.jsx
+++ CarbonCreditTraing/src/App.jsx
@@ -1,15 +1,33 @@
 
-import './App.css'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+import Layout from './Layout'
+import Home from './components/Home'
+
+
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+        path: '/',
+        element: <Home />
+      },
+      ]
+    }
 
+  ])
 
   return (
     <>
-      
+      <RouterProvider router={router}/>
     </>
   )
 }
 
 export default App
+        
---
+++ CarbonCreditTraing/src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+function Home() {
+    return (
+        <>
+            <section id="home" class="hero">
+                <div class="hero-content">
+                    <h1>Transform Your Emissions into Tradeable Assets </h1>
+                    <p>Leverage blockchain technology to monetize your carbon reduction efforts and contribute to global sustainability.</p>
+                    <button class="btn-start">Get Started</button>
+                </div>
+            </section>
+            <section id="features" class="features">
+                <h2>Our Features</h2>
+                <div class="feature-list">
+                    <div class="feature">
+                        <h3>Feature 1</h3>
+                        <p>Description of feature 1.</p>
+                    </div>
+                    <div class="feature">
+                        <h3>Feature 2</h3>
+                        <p>Description of feature 2.</p>
+                    </div>
+                    <div class="feature">
+                        <h3>Feature 3</h3>
+                        <p>Description of feature 3.</p>
+                    </div>
+                </div>
+            </section>
+        </>
+      );
+}
+
+export default Home;
